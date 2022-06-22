a88a7fe7545d5767bfd9813ae0fcc9148e2bfcfe
vannakdy <dyvannak1997@gmail.com>
2022-06-22 20:24:56 +0700

    refresh token

--- src/helpler.js
+++ src/helpler.js
@@ -5,8 +5,11 @@ export const baseUrl = "https://nitc.cleverapps.io/" // production
 // export const baseUrl = "http://localhost:8080/" // developement
 // export const baseUrl = "https://test.com/" // developement
 
-export const fetchData = (paramUrl="",data={},method="GET") => {
-    const token = localStorage.getItem("accessToken")
+export const fetchData = (paramUrl="",data={},method="GET",newRefreshToken=undefined) => {
+    var token = localStorage.getItem("accessToken")
+    if(newRefreshToken != undefined){
+        token = newRefreshToken
+    }
     return axios({
         method : method,
         url : baseUrl+paramUrl,
@@ -17,6 +20,33 @@ export const fetchData = (paramUrl="",data={},method="GET") => {
     }).then(response=>{
         return response.data;
     }).catch(err=>{
+        var status = err.response ?  err.response.status : "";
+        if(status == 403){
+            refreshToken(paramUrl,data,method)
+        }
+    })
+}
+
+export const refreshToken = (paramUrl,paramData,method) => {
+    var refreshToken = localStorage.getItem("refreshToken")
+    return axios({
+        method : "POST",
+        url : baseUrl+"api/auth/refreshToken",
+        data : {
+            token : refreshToken
+        },
+        headers : {
+
+        }
+    }).then(response=>{
+        var data = response.data;
+        var newToken = data.accessToken
+        var newRefreshToken = data.refreshToken
+        localStorage.setItem("accessToken" , newToken);
+        localStorage.setItem("refreshToken" , newRefreshToken);
+        return fetchData(paramUrl,paramData,method,newToken) // recall when have new token
+    }).catch(err=>{
         window.location.href = "/login"
     })
 }
+
